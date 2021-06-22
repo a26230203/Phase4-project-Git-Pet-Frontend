1cc82ef3ddb5f83744967b6cff2c8120baf418e6
Corey Colligan <coreyleec91288@gmail.com>
2021-06-22 18:28:06 -0400

    bin kicked ass

--- git-pet/src/App.js
+++ git-pet/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MainLogin from "./Component/Login"
+import Signup from "./Component/Signup"
+import Home from "./Component/Home"
+import UserLogin from "./Component/UserLogin";
+import BreederLogin from "./Component/BreederLogin";
+
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+          <Route exact path="/" component={(props) => (
+            <MainLogin {...props} />
+          )} />
+
+          <Route exact path="/userlogin" component={(props) => (
+            <UserLogin {...props} />
+          )} />
+
+          <Route exact path="/breederlogin" component={(props) => (
+            <BreederLogin {...props} />
+          )} />   
+
+            <Route exact path="/signup" component={(props) => (
+            <Signup {...props} />
+          )} /> 
+
+            <Route exact path="/home" component={(props) => (
+            <Home {...props} />
+          )} /> 
+          
+
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ git-pet/src/Component/BreederLogin.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class BreederLogin extends Component {
+        
+    handleRegister = () => {
+        this.props.history.push("/signup")
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input 
+                    type="text"
+                    name="username"
+                    value=""
+                    placeholder="Enter your username"/>
+                    <br/>
+                    <input 
+                    type="password"
+                    name="password"
+                    value=""
+                    placeholder="Enter your passwords"/>
+                    <br/>
+                    <button> Login </button>
+                </form>
+                <button onClick={() => this.handleRegister()}>Signup</button>
+            </div>
+        )
+    }
+}
